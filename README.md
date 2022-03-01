737858cb2f2ca9472f20f5ee39b670e4afadb098
Marcus Weinberger <marcusjw.ftw@gmail.com>
2022-03-01 14:02:31 -0500

    Moving to the cryptography library

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # w3backups
 
+<img src="icon.png"></img>
+
 `w3backups` is a program that uses the [web3.storage](https://web3.storage) service through the [API](https://pypi.org/project/web3storage/).
+
+This project is still in development and currently is not working.
 
 # config
 
 Check `config.json` before running, make sure you enter your API key from web3.storage and that the list of files to backup is populated. 
+
+# requirements
+
+`w3backups` requires python>=3.9, along with several python libraries that can be installed with `pip`.
+
+`pip install web3storage rich cryptography`
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from web3storage import Client as Web3Client
-from Crypto.PublicKey import RSA
 from datetime import datetime
 from zipfile import ZipFile
 from pathlib import Path
@@ -7,16 +6,36 @@ from rich import print
 import json
 import os
 
+# cryptography imports
+import cryptography
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
 def load_key():
-    if not Path('key.rsa').is_file():
-        print("[bold red]Key not found[/bold red]\nGenerating key...")
-        key = RSA.generate(4096)
-        with open('key.rsa', 'wb') as key_file:
-            key_file.write(key.exportKey())
-        print("[bold green]Key generated[/bold green]\nSaved to [italic]key.rsa[/italic]")
+    if not Path('key.pem').is_file():
+        print("[bold red]Key not found[/bold red]\nGenerating key (please wait)...")
+        key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=4096, # need bigger key to enc more bytes
+            backend=default_backend()
+        )
+        with open('key.pem', 'wb') as key_file:
+            key_file.write(key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            ))
+        print("[bold green]Key generated[/bold green]\nSaved to [italic]key.pem[/italic]")
     else:
-        with open('key.rsa', 'rb') as key_file:
-            key = RSA.importKey(key_file.read())
+        with open('key.pem', 'rb') as key_file:
+            key = serialization.load_pem_private_key(
+                key_file.read(),
+                password=None,
+                backend=default_backend()
+            )
     return key
 
 def load_config():
@@ -24,21 +43,21 @@ def load_config():
         config = json.load(config_file)
     return config
 
+def add_to_zip(file_obj, zip_file):
+    print('[bold blue]Adding ' + file_obj.name + ' to backup...[/bold blue]')
+    if file_obj.is_dir():
+        for f in file_obj.iterdir():
+            add_to_zip(f, zip_file)
+    else:
+        zip_file.write(str(file_obj)) # str gets the path
+        print('[grey]Added[/grey] ' + str(file_obj))
+
 def create_zip(dirs):
     print('[bold blue]Creating backup...[/bold blue]')
     out_file = 'backup_' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.zip'
     zipObj = ZipFile(out_file, 'w')
-    for dirn in dirs:
-        fileObj = Path(dirn)
-        if fileObj.is_dir():
-            for child in fileObj.iterdir():
-                if child.is_dir():
-                    continue
-                zipObj.write(child.read_bytes(), child.name)
-                print('[grey]Added[/grey] ' + child.name)
-        else:
-            zipObj.write(dirn.read_bytes(), dirn.name)
-            print('[grey]Added[/grey] ' + dirn.name)
+    for d in dirs:
+        add_to_zip(Path(d), zipObj)
     zipObj.close()
     return out_file
 
@@ -46,7 +65,14 @@ def encrypt_backup(filename, key):
     print('[bold blue]Encrypting backup...[/bold blue]')
     with open(filename, 'rb') as f:
         data = f.read()
-    enc = key.encrypt(data, 64)[0]
+    enc = key.public_key().encrypt(
+        data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
     with open('encrypted_' + filename, 'wb') as f:
         f.write(enc)
     return 'encrypted_' + filename
@@ -64,8 +90,15 @@ def restore_last_backup(client: Web3Client, key):
         cid = f.readlines()[-1].strip()
     with open('last_backup.zip', 'wb') as f:
         enc = client.download_file(cid)
-        f.write(key.decrypt(enc))
-    print('[bold green]Restored backup[/bold green]')
+        f.write(key.decrypt(
+            enc,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        ))
+    print('[bold green]Restored backup to [italic]last_backup.zip[/italic][/bold green]')
 
 if __name__ == '__main__':
     key = load_key()
